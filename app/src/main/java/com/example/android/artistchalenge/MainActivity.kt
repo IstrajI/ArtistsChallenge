8fcf853a12139abc29ceee6096ddb084d45cb9e6
Straj <Mr.Straj@yandex.ru>
2022-01-07 00:06:32 +0200

    Finished apollo setup Performed test query

--- app/src/main/java/com/example/android/artistchalenge/MainActivity.kt
+++ app/src/main/java/com/example/android/artistchalenge/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.android.artistchalenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import app.src.main.graphql.com.example.android.artistchalenge.ArtistQuery
+import com.apollographql.apollo3.ApolloClient
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apolloClient = ApolloClient.Builder()
+            .serverUrl("https://graphbrainz.herokuapp.com/")
+            .build()
+
+        MainScope().launch {
+            val response = apolloClient.query(ArtistQuery()).execute()
+            Log.d("TestPish", "response ${response.data?.node}")
+        }
     }
 }
